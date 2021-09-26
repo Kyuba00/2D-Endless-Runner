56abca04a239a7e9b83b32787de7feee01362b0a
Kyuba00 <akanime1@gmail.com>
2021-09-26 20:38:41 +0700

    Initial commit

---
+++ Assets/Script/CameraMoveController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveController : MonoBehaviour
+{
+    [Header("Position")]
+    public Transform player;
+    public float horizontalOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //kamera digerakan dengan mengambil posisi player
+        //dan mengkalkulasikannya dengan offset posisi kamera yang diinginkan
+        Vector3 newPosition = transform.position;
+        newPosition.x = player.position.x + horizontalOffset;
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Script/CharacterMoveController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+    [Header("Jump")]
+    public float jumpAccel;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+    private float lastPositionX;
+
+    private Rigidbody2D rig;
+    private Animator anim;
+    private CharacterSoundController sound;
+
+    private bool isJumping;
+    private bool isOnGround;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sound = GetComponent<CharacterSoundController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //read input
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+
+                //memutar suara pada saat mouse di klik
+                sound.PlayJump();
+            }
+        }
+
+        //change animation
+        anim.SetBool("isOnGround", isOnGround);
+
+        //calculate score
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+
+        if (scoreIncrement > 0)
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+    }
+
+    //dijalan tiap waktu yang fixed
+    private void FixedUpdate()
+    {
+        //raycast ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+
+        //calculate velocity vector
+        Vector2 velocityVector = rig.velocity;
+
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        rig.velocity = velocityVector;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
+    }
+}
